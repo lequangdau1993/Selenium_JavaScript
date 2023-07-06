733f4b1811e04d49950303822fd93c27058e2e90
Dau Le <lequangdau1993@gmail.com>
2023-07-06 09:25:28 +0700

    udpate test.conf.js

--- test.conf.js
+++ test.conf.js
@@ -4,8 +4,10 @@ exports.config = {
         './test_scripts/saucedemo/test_script/testPurchase.js',
         './test_scripts/saucedemo/test_script/testLogin.js',
     ],
-    host: 'localhost',
-    port: 9515,
+    /**This config for Selenium Server (can run on both localhost and remote machine) **/
+    // host: 'localhost',
+    // port: 9515,
+    /********************************************************************************** */
     path:'/',
     capabilities: [
         {
@@ -19,4 +21,14 @@ exports.config = {
     },
     logLevel: 'info',
     reporters: ['spec'],
+    outputDir: 'all-logs',
+    /**     this use for Selenium service (only run on localhost)     **/
+    services: [
+        ['chromedriver', {
+            logFileName: 'wdio-chromedriver.log', // default
+            outputDir: 'driver-logs', // overwrites the config.outputDir
+            args: ['--silent']
+        }]
+    ],
+    /***************************************************************** */
 }
